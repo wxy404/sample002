52c113e0404e9d6ebd2b73aac47abc1d81b9f886
Wei Xiaoyu <wxy_404@github.com>
2019-06-18 17:35:13 +0900

    プロジェクト ファイルを追加します。

---
+++ sample002/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //0~4まで5個分
+        for(int i = 0; i < 5; i++)
+            {
+                for (int n = 0; n <= i; n++)
+                {
+                    Console.Write("■");
+                }
+                Console.WriteLine("");
+            }
+
+            // a の値を表示する
+
+        }
+    }
+}
